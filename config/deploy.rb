ef66fe6dc5434f468b07da6af0a6e5aeaa2d92d5
Sam Corl <sam@samcorl.com>
2016-11-11 21:43:21 -0800

    Initial updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock '3.6.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'server_status'
+set :repo_url, 'git@github.com:samcorl/server_status.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
