23317dbb67ba2018f4e8b618f5138f7d35132833
Sam Corl <sam@samcorl.com>
2016-11-14 12:42:49 -0800

    Remove YUI reference for compressor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,9 +36,6 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
-  config.assets.css_compressor = :yui
-  config.assets.js_compressor = :uglifier
-
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
